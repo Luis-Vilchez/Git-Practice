5a19f6047850fb4dcd6d9eef83ad4549c606e7d3
Luis-Vilchez <107430792+Luis-Vilchez@users.noreply.github.com>
2022-06-24 10:17:01 -0600

    mongo_done

--- mongo.js
+++ mongo.js
@@ -1,7 +1,33 @@
 const mongoose = require("mongoose");
-const powerLifterDB = mongoose.connection
+const powerLifterDB = mongoose.connection;
 
 function connect(user,password,host,port,powerLifterDB){
     const connectionString = ('mongodb+srv://powerlifter:tuco10@cluster0.rrj9vdy.mongodb.net/?retryWrites=true&w=majority');
-    
+    mongoose.connect(connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
 }
+
+function setUpConnectionHandlers(callback){
+    powerLifterDB.once("connecting", () => {
+        console.log("connecting to mongoDB");
+    });
+
+    powerLifterDB.once("connected", () => {
+        console.log("connected to mongoDB");
+    });
+
+    powerLifterDB.once("open", () => {
+        console.log("open connection to mongoDB");
+    });
+
+    powerLifterDB.once("error", () => {
+        console.log("error connecting to mongoDB");
+    });
+}
+
+module.exports = {
+    connect: connect,
+    setUpConnectionHandlers: setUpConnectionHandlers,
+};
