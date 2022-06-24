31bd26226dd81c950b463bade7d0e393184c128e
Luis-Vilchez <107430792+Luis-Vilchez@users.noreply.github.com>
2022-06-24 10:08:58 -0600

    mongo_start

---
+++ mongo.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+const powerLifterDB = mongoose.connection
+
+function connect(user,password,host,port,powerLifterDB){
+    const connectionString = ('mongodb+srv://powerlifter:tuco10@cluster0.rrj9vdy.mongodb.net/?retryWrites=true&w=majority');
+    
+}
