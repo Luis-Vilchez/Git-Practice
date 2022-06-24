5d4d5ffee165ea58bb861d505e16b6e0c6b891d9
Luis-Vilchez <107430792+Luis-Vilchez@users.noreply.github.com>
2022-06-24 10:42:53 -0600

    index_mid

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 const express = require ("express");
+const mongo = require("./mongo");
 const app = express();
-
+//use express
 app.use(express.json());
+
+const mongodb = require("./mongo");
+mongodb.setUpConnectionHandlers(() => {
+    console.log("hi");
+});
+
+mongodb.connect()
+
+// add in env vars
+const dotenv = require("dotenv");
+const {mongo} = require("mongoose");
+
+
+//set up the port number
+const port = flags.get("port") || process.env.PORT || 3000;
+
